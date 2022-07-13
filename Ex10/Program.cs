ce36248da1e5e05d3c213e384f5c3037ce082be8
Julia Pokrovskaya <camomile0@yandex.ru>
2022-07-13 14:15:42 +0300

    Added programs for Ex10, Ex13, Ex15

---
+++ Ex10/Program.cs
@@ -0,0 +1,6 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int number2 = (number % 100 - number % 10) / 10;
+Console.WriteLine(number2);
---
+++ Ex13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string NumberLength = Convert.ToString(number);
+if (NumberLength.Length > 2)
+{
+    Console.WriteLine(NumberLength[2]);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
